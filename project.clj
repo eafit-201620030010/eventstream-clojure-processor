3c29839c43bb9a6a90d6ffcd4457f5552894945e
jjchavarrg <jjchavarrg@eafit.edu.co>
2023-11-16 21:00:54 -0500

    Config aliases project

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
                  [org.apache.kafka/kafka-clients "2.8.0"]
                  [clj-http "3.12.0"]
                  [cc.qbits/spandex "0.8.2"]]
+  :aliases {"wikimedia-stream" ["run" "-m" "eventstream-clojure-processor.eventstreams.main"]
+            "consumer-kafka-elastic" ["run" "-m" "eventstream-clojure-processor.elasticsearch.elasticsearch-main"]}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
